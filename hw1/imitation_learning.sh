84474d2b6f2fe1053e7258cd723ad6e054c13572
InnerPeace-Wu <innerpeace.wu@outlook.com>
2017-12-30 14:08:46 +0800

    update hw1

--- hw1/imitation_learning.sh
+++ hw1/imitation_learning.sh
@@ -9,7 +9,7 @@ LOG="output/bc_log.txt.`date +%Y-%m-%d_%H-%M-%S`"
 exec &> >(tee -a "$LOG")
 echo Logging output to "$LOG"
 
-time python bc_tf.py \
+time python imitation_learning.py \
     $1 \
     --batch_size 32 \
     --epoch 100
